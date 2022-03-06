175480964e1b3d16da65ac5da5322fcd8c58bd7c
Leda <ledathemis@protonmail.com>
2022-03-06 07:47:03 +0300

    add rows to make a square grid

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function createGrids(n) {
   const gridContainer = document.querySelector("#grid-container");
   for (let i = 1; i <= n; i++) {
-    let grid = document.createElement("div");
-    grid.id = `grid-${i}`;
-    grid.classList.add("grid-block");
-    grid.style.width = "16px";
-    grid.style.height = "16px";
-    gridContainer.appendChild(grid);
+    let row = document.createElement("div");
+    row.id = `row-${i}`;
+    row.classList.add("grid-row");
+
+    for (let j = 1; j <= n; j++) {
+      let grid = document.createElement("div");
+
+      grid.id = `grid-${j + i * 16 - 16}`;
+      grid.classList.add("grid-block");
+      grid.style.width = "16px";
+      grid.style.height = "16px";
+
+      row.appendChild(grid);
+    }
+
+    gridContainer.appendChild(row);
   }
 }
 
