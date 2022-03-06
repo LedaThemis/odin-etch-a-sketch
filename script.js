981f872ba162c83c18a6b71bf00382787755b174
Leda <ledathemis@protonmail.com>
2022-03-06 07:37:21 +0300

    style grid-blocks

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function createGrids(n) {
   for (let i = 1; i <= n; i++) {
     let grid = document.createElement("div");
     grid.id = `grid-${i}`;
+    grid.classList.add("grid-block");
     grid.style.width = "16px";
     grid.style.height = "16px";
-    grid.style.border = "2px solid grey";
     gridContainer.appendChild(grid);
   }
 }
